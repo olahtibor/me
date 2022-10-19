9125be68e4f370a42a4fc2313a1df35f607f805f
olahtibor <60151970+olahtibor@users.noreply.github.com>
2022-10-19 14:12:28 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Seo Marketing">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div style="width:100%;background-image: url('back.jpg');color:white;">
+<center><h1>The true...</h1></center>
+</div>
+<body>
+</html>
